18ea38fe90e440df32c236492940f3cb9c0cfa7b
BLoHny <113517233+BLoHny@users.noreply.github.com>
2023-12-20 11:01:24 +0900

    final

--- main.py
+++ main.py
@@ -21,14 +21,14 @@ def test(books: list[Book]):
     for book in books:  # Fix 1: Loop through books directly
         j = hi(book)
 
-        if j == 1:
+        if j == 1:  
             book_detail_data = {"bookId": book.bookId}
             book_detail = BookDetail(**book_detail_data)
             data.append(book_detail.dict())
 
-    spring_server_url = "http://localhost:9039/hi/test"  # Spring 서버의 URL로 변경
+    spring_server_url = "http://localhost:9039/hi"  # Spring 서버의 URL로 변경
     print(data)
-    response = requests.get(spring_server_url, json={"data": data})
+    response = requests.post(spring_server_url, json=data)
 
     if response.status_code != 200:
         raise HTTPException(status_code=500, detail="Failed to send data to Spring server")
@@ -39,7 +39,7 @@ def hi(book: Book):  # Fix 2: Use 'book' instead of 'Book'
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": book.content + " 이 문장이 유해한 단어나 욕설을 포함한다면, 0을 출력해줘 다른 이상한 말은 석지말고, 유해한 단어나 욕설이 없다면 1을 출력해줘 다른 이상한 말은 석지말고, 제발 숫자만 출력해줘 답변에 숫자만 출력해라"}
+            {"role": "user", "content": book.content + " 이 문장이 유해한 단어나 욕설을 포함한다면, 0을 출력해줘 다른 이상한 말은 석지말고, 유해한 단어나 욕설이 없다면 1을 출력해줘 다른 이상한 말은 석지말고, 답변에 숫자만 출력해"}
         ]
     )
 
